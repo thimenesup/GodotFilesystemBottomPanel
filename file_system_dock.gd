d7990396386edd6e54a65b21e03f867ed6647117
thimenesup <thimenesup@gmail.com>
2021-12-18 22:43:00 +0100

    Fix crash, implement optional first button placement

--- file_system_dock.gd
+++ file_system_dock.gd
@@ -3,21 +3,37 @@ extends EditorPlugin
 
 const PluginName := "FileSystemBottomPanel"
 
+const FirstPlaceButton := true
+
 var _file_system_node_name := "FileSystem"
 var _file_system_dock: Control = null
+var _tool_button: ToolButton = null
+var _original_index: int = -1
 
-func _enter_tree() -> void:
+func _ready() -> void:
+	#Wait a frame to ensure that the editor has correctly initialized the file system to prevent crashes on boot
+	yield(get_tree(), "idle_frame")
+	
 	_file_system_dock = find_file_system_dock_node()
 	if not _file_system_dock:
 		printerr("Couldn't find Filesystem Dock")
 		return
 	
 	remove_control_from_docks(_file_system_dock)
-	add_control_to_bottom_panel(_file_system_dock, _file_system_dock.name)
+	_tool_button = add_control_to_bottom_panel(_file_system_dock, _file_system_dock.name)
+	_original_index = _tool_button.get_index()
+	
+	#Make the FileSystem button to appear first in the bottom panel
+	if FirstPlaceButton:
+		_tool_button.get_parent().move_child(_tool_button, 0)
 
 func _exit_tree() -> void:
 	if not _file_system_dock:
 		return
+	
+	#The editor expects the original index, so move it back for proper deinitialization
+	if _tool_button.get_index() != _original_index:
+		_tool_button.get_parent().move_child(_tool_button, _original_index)
 	
 	remove_control_from_bottom_panel(_file_system_dock)
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, _file_system_dock)
@@ -26,7 +42,7 @@ func _exit_tree() -> void:
 func get_plugin_name() -> String:
 	return PluginName
 
-func get_plugin_icon() -> Object: #Texture
+func get_plugin_icon() -> Texture:
 	return get_editor_interface().get_base_control().get_icon("File", "EditorIcons")
 
 
